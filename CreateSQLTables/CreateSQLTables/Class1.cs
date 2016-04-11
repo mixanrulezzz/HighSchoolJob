6a9264176e80430882618b4934132c308a83146d
mixanrulezzz <mikhail4782010@yandex.ru>
2016-04-11 17:38:27 +0400

    Обновил библиотеку для создания таблиц

--- CreateSQLTables/CreateSQLTables/Class1.cs
+++ CreateSQLTables/CreateSQLTables/Class1.cs
@@ -10,12 +10,12 @@ namespace CreateSQLTables
 {
     public static class CreateTable
     {
-        public static void Bagage(SQLiteConnection File)
+        public static void Product(SQLiteConnection File)
         {
             try
             {
                 SQLiteCommand create = new SQLiteCommand(File);
-                create.CommandText = "create table Bagage(bagid INTEGER PRIMARY KEY, bagName VARCHAR(50) NOT NULL, Price INTEGER NOT NULL, PhotoLink varvhar(100) NOT NULL);";
+                create.CommandText = "create table Product(prodid INTEGER NOT NULL, prodName VARCHAR(50) NOT NULL, Price INTEGER NOT NULL, PhotoLink varvhar(100) NOT NULL, CONSTRAINT ProdPK PRIMARY KEY (prodid));";
                 File.Open();
                 create.ExecuteNonQuery();
             }
@@ -27,31 +27,31 @@ namespace CreateSQLTables
             try
             {
                 SQLiteCommand create = new SQLiteCommand(File);
-                create.CommandText = "create table Client(clid INTEGER PRIMARY KEY, clName VARCHAR(15) NOT NULL, clAge INTEGER NOT NULL);";
+                create.CommandText = "create table Client(clid INTEGER NOT NULL, clName VARCHAR(15) NOT NULL, CONSTRAINT ClientPK PRIMARY KEY (clid));";
                 File.Open();
                 create.ExecuteNonQuery();
             }
             catch (Exception) { }
         }
 
-        public static void Creator(SQLiteConnection File)
+        public static void Provider(SQLiteConnection File)
         {
             try
             {
                 SQLiteCommand create = new SQLiteCommand(File);
-                create.CommandText = "create table Creator(crid INTEGER PRIMARY KEY, crName VARCHAR(15) NOT NULL);";
+                create.CommandText = "create table Provider(provid INTEGER NOT NULL, provName VARCHAR(15) NOT NULL, CONSTRAINT ProvPK PRIMARY KEY (provid));";
                 File.Open();
                 create.ExecuteNonQuery();
             }
             catch (Exception) { }
         }
 
-        public static void BagageCreator(SQLiteConnection File)
+        public static void ProductProvider(SQLiteConnection File)
         {
             try
             {
                 SQLiteCommand create = new SQLiteCommand(File);
-                create.CommandText = "create table BagageCreator(bagid INTEGER NOT NULL, crid INTEGER NOT NULL, CONSTRAINT BagCrPK PRIMARY KEY (bagid, crid));";
+                create.CommandText = "create table ProductProvider(prodid INTEGER NOT NULL, provid INTEGER NOT NULL, CONSTRAINT ProdProvPK PRIMARY KEY (prodid, provid), CONSTRAINT ProdFK FOREIGN KEY (prodid) REFERENCES Product (prodid) ON DELETE CASCADE, CONSTRAINT ProvFK FOREIGN KEY (provid) REFERENCES Provider (provid) ON DELETE CASCADE);";
                 File.Open();
                 create.ExecuteNonQuery();
             }
@@ -63,7 +63,7 @@ namespace CreateSQLTables
             try
             {
                 SQLiteCommand create = new SQLiteCommand(File);
-                create.CommandText = "create table ByingHistoory(bhid INTEGER PRIMARY KEY, bagid INTEGER NOT NULL, clid INTEGER NOT NULL, count INTEGER NOT NULL, );";
+                create.CommandText = "create table ByingHistory(bhid INTEGER NOT NULL, prodid INTEGER NOT NULL, clid INTEGER NOT NULL, count INTEGER NOT NULL, HistoryDate DATE NOT NULL, CONSTRAINT bhPK PRIMARY KEY (bhid), CONSTRAINT ProdFK FOREIGN KEY (prodid) REFERENCES Product (prodid) ON DELETE CASCADE, CONSTRAINT ClientFK FOREIGN KEY (clid) REFERENCES Client (clid) ON DELETE CASCADE);";
                 File.Open();
                 create.ExecuteNonQuery();
             }
