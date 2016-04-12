ac8da2a681db732736fb6d803352ca90da5f5ff0
mixanrulezzz <mikhail4782010@yandex.ru>
2016-04-12 13:38:21 +0400

    Добавил поиск для таблицы Продукт

--- SearchMaxID/SearchMaxID/Class1.cs
+++ SearchMaxID/SearchMaxID/Class1.cs
@@ -3,15 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SQLite;
 
 namespace SearchMaxID
 {
     static public class SearchMaxID
     {
-        static int Product()
+        static int Product(SQLiteConnection File)
         {
-
-            return 0;
+            int maxID = 0;
+            SQLiteCommand findMaxIndex = new SQLiteCommand(File);
+            findMaxIndex.CommandText = "select max(prodid) from Product";
+            SQLiteDataReader selectMax = findMaxIndex.ExecuteReader();
+            if (selectMax.Read())
+            {
+                var v = selectMax.GetValue(0);
+                try { maxID = Convert.ToInt32(v); }
+                catch (Exception)
+                { maxID = 0; }
+            }
+            return maxID;
         }
+
+        
     }
 }
