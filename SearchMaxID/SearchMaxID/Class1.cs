8b838abf3b18f1f1e9b118ca5d0351b0bf06dae2
mixanrulezzz <mikhail4782010@yandex.ru>
2016-04-12 13:21:07 +0400

    Создал класс на поиск максимального id

---
+++ SearchMaxID/SearchMaxID/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SearchMaxID
+{
+    static public class SearchMaxID
+    {
+        static int Product()
+        {
+
+            return 0;
+        }
+    }
+}
