12db4d6e04f570c976f1fc0382ecc75b33d9a724
mixanrulezzz <mikhail4782010@yandex.ru>
2016-04-29 21:25:17 +0400

    Добавил классы Поставщик и Список поставщиков
    Добавил классы Поставщик и Список поставщиков, в классе список
    поставщиков написал индексатор и поле Count которое возвращает
    количество объектов в списке

---
+++ ProviderLibrary/ProviderLibrary/Class1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProviderLibrary
+{
+    public class Provider
+    {
+        public int ProviderID;
+        public string ProviderName;
+
+        public Provider()
+        { }
+
+        public Provider(int ProviderID, string ProviderName)
+        {
+            this.ProviderID = ProviderID;
+            this.ProviderName = ProviderName;
+        }
+
+        public override string ToString()
+        {
+            return ProviderID.ToString() + " " + ProviderName;
+        }
+    }
+
+    public class ProviderList
+    {
+        private List<Provider> Providers;
+
+        public ProviderList()
+        {
+            Providers = new List<Provider>();
+        }
+
+        public int Add(Provider NewProvider)
+        {
+            if (NewProvider.ProviderName.Length > 40)
+                return 1;
+            Providers.Add(NewProvider);
+            return 0;
+        }
+
+        public int Add(int ProviderID, string ProviderName)
+        {
+            if (ProviderName.Length > 40)
+                return 1;
+            Providers.Add(new Provider(ProviderID, ProviderName));
+            return 0;
+        }
+
+        public bool Remove(Provider DelProvider)
+        {
+            try { Providers.Remove(DelProvider); }
+            catch (Exception) { return false; }
+            return true;
+        }
+
+        public bool RemoveAt(int ProviderIndex)
+        {
+            try { Providers.RemoveAt(ProviderIndex); }
+            catch (Exception) { return false; }
+            return true;
+        }
+
+        public Provider this[int i]
+        {
+            get { return Providers[i]; }
+            set { Providers[i] = value; }
+        }
+
+        public int Count
+        {
+            get { return Providers.Count; }
+        }
+    }
+}
