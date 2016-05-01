04fdef2f34e16376fec7c2fcba1f6d1309459eec
mixanrulezzz <mikhail4782010@yandex.ru>
2016-05-01 23:04:19 +0400

    Начал писать вывод из списка
    Начал писать вывод из списка и нашел небольшие недостатки при начале
    работы программы, которые нужно исправить

---
+++ OutputList/OutputList/Class1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ShipmentLibrary;
+using ClientLibrary;
+using ProviderLibrary;
+using ProviderShipmentLibrary;
+using PurchaseHistoryLibrary;
+using System.Data.SQLite;
+
+namespace OutputList
+{
+    public static class OutputList
+    {
+        public static void ListOfShipments(SQLiteConnection DataFile, ShipmentList List)
+        {
+
+        }
+
+        public static void ListOfClients(SQLiteConnection DataFile, ClientList List)
+        {
+
+        }
+
+        public static void ListOfProviders(SQLiteConnection DataFile, ProviderList List)
+        {
+
+        }
+
+        public static void ListOfProviderShipments(SQLiteConnection DataFile, ProviderShipmentList List)
+        {
+
+        }
+
+        public static void ListOfPurchaseHistory(SQLiteConnection DataFile, PurchaseHistoryList List)
+        {
+
+        }
+    }
+}
