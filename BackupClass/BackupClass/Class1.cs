8bf72001ab4c2d249388e95a8c98c1f7a43bc729
Karpov M.A <karpovprog@yandex.ru>
2016-08-22 14:38:29 +0400

    Создал класс для того, чтобы работала резервная копия

---
+++ BackupClass/BackupClass/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClientLibrary;
+using ShipmentLibrary;
+using ProviderLibrary;
+using ProviderShipmentLibrary;
+using PurchaseHistoryLibrary;
+
+namespace BackupClass
+{
+    public class BackupClass
+    {
+        public ClientList Clients;
+        public ProviderList Providers;
+        public ShipmentList Shipments;
+        public ProviderShipmentList ProviderShipment;
+        public PurchaseHistoryList PurchaseHistory;
+
+        public BackupClass(ClientList Clients, ProviderList Providers, ShipmentList Shipments, ProviderShipmentList ProviderShipment, PurchaseHistoryList PurchaseHistory)
+        {
+            this.Clients = Clients;
+            this.Providers = Providers;
+            this.Shipments = Shipments;
+            this.ProviderShipment = ProviderShipment;
+            this.PurchaseHistory = PurchaseHistory;
+        }
+    }
+}
