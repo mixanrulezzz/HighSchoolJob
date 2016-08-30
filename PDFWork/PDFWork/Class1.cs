2cf3ace04652d17400d6fe08f14d4a2f2c030946
Karpov M.A <karpovprog@yandex.ru>
2016-08-30 21:23:57 +0400

    Начал работу с созданием чека и накладной

--- PDFWork/PDFWork/Class1.cs
+++ PDFWork/PDFWork/Class1.cs
@@ -3,11 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using iTextSharp;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System.IO;
 
 namespace PDFWork
 {
     public static class PDF
     {
+        public static bool CreatePackingList(string file, string date)
+        {
+            var doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(file, FileMode.Create));
+            doc.Open();
+            return true;
+        }
 
+        public static bool CreateCheck(string file, string date)
+        {
+            var doc = new Document();
+            PdfWriter writer = PdfWriter.GetInstance(doc, new FileStream(file, FileMode.Create));
+            doc.Open();
+            return true;
+        }
     }
 }
